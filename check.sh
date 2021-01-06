f016dd5c0abd20126e97e8f915fc06b7ccc867ac
wangyiqi <1150260034@qq.com>
2021-01-06 11:12:50 +0800

    取消了网络检查

--- check.sh
+++ check.sh
@@ -60,39 +60,6 @@ vmstat -a -S m 1 2 >>$(dirname $0)/checkOSMON.log
 
 echo "" >>$(dirname $0)/checkOSMON.log
 
-echo -e "\033[33m************************检查网络稳定性**********************************\033[0m">>$(dirname $0)/checkOSMON.log
-
-echo -e "\033[36m##############检查公共网络192.168.168.21/22##############\033[0m">>$(dirname $0)/checkOSMON.log
-
-echo `/usr/bin/date "+%Y-%m-%d %H:%M:%S"` >> $(dirname $0)/checkOSMON.log
-
-traceroute -r -F -s 192.168.10.147 192.168.10.20 >>$(dirname $0)/checkOSMON.log
-
-echo `/usr/bin/date "+%Y-%m-%d %H:%M:%S"` >> $(dirname $0)/checkOSMON.log
-
-traceroute -r -F -s 192.168.10.147 192.168.10.20 >>$(dirname $0)/checkOSMON.log
-
-echo `/usr/bin/date "+%Y-%m-%d %H:%M:%S"` >> $(dirname $0)/checkOSMON.log
-
-traceroute -r -F -s 192.168.10.147 192.168.10.20 >>$(dirname $0)/checkOSMON.log
-
-echo "" >>$(dirname $0)/checkOSMON.log
-
-echo -e "\033[36m###############检查私有网络10.1.1.1/2##############\033[0m">>$(dirname $0)/checkOSMON.log
-
-echo `/usr/bin/date "+%Y-%m-%d %H:%M:%S"` >> $(dirname $0)/checkOSMON.log
-
-traceroute -r -F -s 172.16.84.167 172.16.84.132 >>$(dirname $0)/checkOSMON.log
-
-echo `/usr/bin/date "+%Y-%m-%d %H:%M:%S"` >> $(dirname $0)/checkOSMON.log
-
-traceroute -r -F -s 172.16.84.167 172.16.84.132 >>$(dirname $0)/checkOSMON.log
-
-echo `/usr/bin/date "+%Y-%m-%d %H:%M:%S"` >> $(dirname $0)/checkOSMON.log
-
-traceroute -r -F -s 172.16.84.167 172.16.84.132 >>$(dirname $0)/checkOSMON.log
-
-echo "" >>$(dirname $0)/checkOSMON.log
 
 echo -e "\033[36m##############读取网卡流量信息:##############\033[0m" >>$(dirname $0)/checkOSMON.log
 
